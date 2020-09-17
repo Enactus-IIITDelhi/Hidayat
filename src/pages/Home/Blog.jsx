784b47d3f2b0a1be2298a4438234f65b0ef1c2c5
nandikajain <jainnandika@gmail.com>
2020-09-18 02:57:24 +0530

    Minor fixes in blog #4

--- src/pages/Home/Blog.jsx
+++ src/pages/Home/Blog.jsx
@@ -7,19 +7,22 @@ function Blog()
 {
 
   return(
-      <div className="container-fluid pt-5 pb-2 bg-soft-grey ">
-
-        <div className="row m-auto ">
-          <div className="col-10 offset-1 p-0" >
+    <div className="container-fluid pt-5 pb-2 bg-soft-grey ">
+      <div className="row m-auto ">
+          <div className="col-11 offset-1 col-md-5 p-0">
+          </div>
+          <div className="col-10 offset-1 offset-md-0 col-md-5  p-0" >
             <h2 className="text-main-blue m-0 p-0 text-right" >Blogs.</h2>
             <hr className="ruler pull-right m-0 mt-2" />
           </div>
-
-          <div className="col-12 col-md-10 offset-md-1">
-            <BlogsCarousel />
-          </div>
+          <div className="col-sm-1"></div>
+        </div>
+      <div className="row m-auto ">
+        <div className="col-12 col-md-10 offset-md-1">
+          <BlogsCarousel />
         </div>
       </div>
+    </div>
     );
 }
 
@@ -41,29 +44,29 @@ function BlogsCarousel(props) {
           slidesPerPage: 2,
           arrows: true,
         }
-        
+
       }}>
 
       <BlogCard storyNo="1" />
       <BlogCard storyNo="2" />
       <BlogCard storyNo="3" />
       <BlogCard storyNo="4" />
-      
+
     </Carousel>
   );
 }
 
 function BlogCard(props) {
   return (
-    <div className="pl-md-4 pr-md-4 pt-5 pb-5 ml-1 mr-1">
+    <div className="pl-md-2 pr-md-2 pt-5 pb-5 ml-1 mr-1">
       <Card className="border border-dark">
         <CardImg className="border-bottom border-dark" top width="100%" src="/assets/HomePage/dummy.png" alt="Card image cap" />
         <CardBody>
-          <CardTitle className="h2">Stroy {props.storyNo}</CardTitle>
+          <CardTitle className="h3">Story {props.storyNo}</CardTitle>
           <CardText>
-            Nisi proident adipisicing nisi velit dolor exercitation pariatur in enim enim. Minim quis labore sunt dolore sit occaecat magna ea nulla. Sit laboris eu fugiat ea tempor laboris fugiat enim minim tempor. Commodo ipsum incididunt fugiat pariatur non cupidatat. 
+            Nisi proident adipisicing nisi velit dolor exercitation pariatur in enim enim. Minim quis labore sunt dolore sit occaecat magna ea nulla. 
           </CardText>
-          <Button size="lg" color="light" outline className="bg-main-blue pull-right border-radius-30 pl-4 pr-4">Button</Button>
+          <Button className="bg-main-blue  pull-right" style={ {borderRadius:"30px"}} > Checkout </Button>
         </CardBody>
       </Card>
     </div>
