e3b5c0d595b82732ce35c29235a1f0f04108e1ee
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-12-27 15:18:15 +0530

    Updates

---
+++ precache-manifest.707ef350c2ed33eecdabab5f7bde70fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cd8240299734bc2b627db25c1722ef",
+    "url": "/Hidayat/index.html"
+  },
+  {
+    "revision": "f58cef5f4a892a3c2fff",
+    "url": "/Hidayat/static/css/2.ed0364cd.chunk.css"
+  },
+  {
+    "revision": "2822a59d7ef2b908105f",
+    "url": "/Hidayat/static/css/main.1e70cd55.chunk.css"
+  },
+  {
+    "revision": "f58cef5f4a892a3c2fff",
+    "url": "/Hidayat/static/js/2.646f0ea0.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/Hidayat/static/js/2.646f0ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2822a59d7ef2b908105f",
+    "url": "/Hidayat/static/js/main.c9d66d7e.chunk.js"
+  },
+  {
+    "revision": "c61195c2d5a76b0c5493",
+    "url": "/Hidayat/static/js/runtime-main.dc21f9e3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Hidayat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Hidayat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Hidayat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Hidayat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Hidayat/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a52233d9166720b1231d566a7cb4623.js"
+  "/Hidayat/precache-manifest.707ef350c2ed33eecdabab5f7bde70fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hidayat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
