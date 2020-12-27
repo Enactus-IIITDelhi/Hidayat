5303974e83e5c822d623c05e7f8e2d990f4402bf
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-12-27 14:50:00 +0530

    Updates

---
+++ precache-manifest.6a52233d9166720b1231d566a7cb4623.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602d1ac40cf285da5083fdb5189ceb37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea51463706c2496aeccd",
+    "url": "/static/css/2.1817dc33.chunk.css"
+  },
+  {
+    "revision": "c18cf654625eef14362e",
+    "url": "/static/css/main.1e70cd55.chunk.css"
+  },
+  {
+    "revision": "ea51463706c2496aeccd",
+    "url": "/static/js/2.646f0ea0.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/static/js/2.646f0ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18cf654625eef14362e",
+    "url": "/static/js/main.f34d9949.chunk.js"
+  },
+  {
+    "revision": "898e19bac51208fd9ec1",
+    "url": "/static/js/runtime-main.cd1db577.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
